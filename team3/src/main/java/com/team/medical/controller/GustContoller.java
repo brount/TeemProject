776ed8b7990928b3959088957be0e05bdfcd5220
kosmo-10 <kosmo-10@kosmo-10-PC>
2018-07-12 17:30:32 +0900

    안녕

---
+++ team3/src/main/java/com/team/medical/controller/GustContoller.java
@@ -0,0 +1,18 @@
+package com.team.medical.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GustContoller {
+	@RequestMapping(value = "/")
+	public String home(HttpServletRequest req, Model model) {
+		
+	
+		return null;
+	}
+		
+}
